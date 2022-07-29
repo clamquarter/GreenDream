ad76a9c3a7865d78361cf25832b1a530d30c820e
India Doria <60370426+clamquarter@users.noreply.github.com>
2022-07-29 11:02:09 -0400

    1st commit!
    Co-Authored-By: ngordon68 <102773701+ngordon68@users.noreply.github.com>

---
+++ GreenDream/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GreenDream
+//
+//  Created by India Doria on 7/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var Test: UIButton!
+ 
+}
+
