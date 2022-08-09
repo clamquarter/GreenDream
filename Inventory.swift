2f0f780f9781a66080354ec639379a819f34be21
India Doria <60370426+clamquarter@users.noreply.github.com>
2022-08-09 10:51:53 -0400

    Update Inventory.swift

--- Inventory.swift
+++ Inventory.swift
@@ -4,51 +4,82 @@
 //
 //  Created by Marcey Carroll on 8/1/22.
 //
+//
+//  Inventory.swift
+//  GreenDream
+//
+//  Created by Marcey Carroll on 8/1/22.
+//
 
 import Foundation
+import UIKit
 
-class Food:Identifiable {
-    var id: UUID
+
+class Food: Identifiable, ObservableObject {
+    // var id: UUID
     var name: String?
-    var price: Float
+   @Published var price: Float
     var farm: String?
     var image: String?
-    var qty: Int
-    //var total: Float(qty)
+    @Published var qty: Int
     
-    init(name:String, price:Float, farm:String, image:String)
+    
+    init( name:String, price:Float, farm:String, image:String, qty:Int)
     {
-        self.id = UUID()
+        // self.id = UUID()
         self.name = name
         self.price = price
         self.farm = farm
         self.image = image
-        self.qty = 0
-    }
+        self.qty = qty
+        
+
+            
     
 }
 
-var apples = Food(name: "apples", price: 1, farm: "Blake's", image: "apple")
-var onions = Food(name: "onions", price: 0.60, farm: "Onion Bros.", image: "onion")
-var tomatoes = Food(name: "tomatoes", price: 0.80, farm: "Detroit Farm", image: "tomato")
-var carrots = Food(name: "carrots", price: 3, farm: "J&M Farms", image: "carrot")
-var cucumbers = Food(name: "cucumber", price: 1, farm: "Green Growers", image: "cucumber")
-var blueberries = Food(name: "blueberries", price: 4, farm: "Blue Skies", image: "blueberry")
-var zucchini = Food(name: "zucchini", price: 0.90, farm: "ZJE Farms", image: "zucchini")
-var bananas = Food(name: "bananas", price: 2, farm: "Detroit Farm", image: "banana")
-var strawberries = Food(name: "strawberries", price: 4, farm: "Blue Skies", image: "strawberry")
-var greenbeans = Food(name: "green beans", price: 2, farm: "Green Growers", image: "greenbean")
+}
 
 
-let Inventory = [
+
+
+
+//Inventory
+var apples = Food(name: "apples", price: 1, farm: "Blake's", image: "Apple", qty:1)
+var onions = Food(name: "onions", price: 0.60, farm: "Onion Bros.", image: "Onion", qty:1)
+var tomatoes = Food(name: "tomatoes", price: 0.80, farm: "Detroit Farm", image: "Tomatoe", qty:1)
+var carrots = Food(name: "carrots", price: 3, farm: "J&M Farms", image: "Carrot", qty:1)
+var cucumbers = Food(name: "cucumber", price: 1, farm: "Green Growers", image: "Cucumber", qty:1)
+var blueberries = Food(name: "blueberries", price: 4, farm: "Blue Skies", image: "Blueberry", qty:1)
+var zucchini = Food(name: "zucchini", price: 0.90, farm: "ZJE Farms", image: "Zuc", qty:1)
+var bananas = Food(name: "bananas", price: 2, farm: "Detroit Farm", image: "Bananna", qty:1)
+var strawberries = Food(name: "strawberries", price: 4, farm: "Blue Skies", image: "Strawberry", qty:1)
+var greenbeans = Food(name: "green beans", price: 2, farm: "Green Growers", image: "GreenBean", qty:1)
+
+
+var Inventory = [
 apples, onions, tomatoes, carrots, cucumbers, blueberries, zucchini, bananas, strawberries, greenbeans
 ]
 
-extension Food : Hashable {
-    static func == (lhs: Food, rhs: Food) -> Bool {
-        return lhs.name == rhs.name }
+
     
+
+        
+        
+        
+        
+        
+        // makes class hashable which lets us to compare values in array such as to see if duplicate already exists
+    
+extension Food : Hashable {
+static func == (lhs: Food, rhs: Food) -> Bool {
+    return lhs.name == rhs.name }
+
     func hash(into hasher: inout Hasher) {
         hasher.combine(name)
+      
     }
 }
+
+
+
